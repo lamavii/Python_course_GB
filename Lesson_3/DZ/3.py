66fcf65d54fc41f22915e5c75f39dce4edd7fdfd
Veronika <dthjif56@gmail.com>
2024-07-15 23:38:52 +1000

    new

---
+++ Lesson_3/DZ/3.py
@@ -0,0 +1,19 @@
+# Создайте словарь со списком вещей для похода
+# в качестве ключа и их массой в качестве значения.
+# Определите какие вещи влезут в рюкзак передав его
+# максимальную грузоподъёмность. Достаточно
+# вернуть один допустимый вариант.
+# *Верните все возможные варианты комплектации рюкзака.
+
+
+def pack_backpack(items, max_weight):
+    possible_items = []
+    for item, weight in items.items():
+        if weight <= max_weight:
+            possible_items.append(item)
+            max_weight -= weight
+    return possible_items
+
+items = {'tent': 5, 'water': 3, 'food': 4, 'clothes': 2, 'first aid kit': 1}
+max_weight = 10
+print(pack_backpack(items, max_weight))
