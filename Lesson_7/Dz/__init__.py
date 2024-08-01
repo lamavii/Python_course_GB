4dd6c25f8e3934536a2393c605c608925045e7ea
Veronika <dthjif56@gmail.com>
2024-08-02 02:38:10 +1000

    n

---
+++ Lesson_7/Dz/__init__.py
@@ -0,0 +1,4 @@
+# 3. Соберите из созданных на уроке и в рамках домашнего задания функций пакет для работы с файлами.
+
+__all__ = ['different_files_mod', 'gen_names_mod', 'rename_file_mod',
+           'sort_files_mod', 'sum_files_mod', 'write_file_mod', 'test']
